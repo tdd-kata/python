5784a252d1fff424b5265b00262ae55fa4b2d886
Changsu Im <imcxsu@gmail.com>
2021-04-12 01:09:59 +0900

    add: django-test ch04 - 템플릿을 이용한 메인 페이지 HTML 생성

--- django-test/ch04-philosophy-and-refactoring/superlists/functional_tests.py
+++ django-test/ch04-philosophy-and-refactoring/superlists/functional_tests.py
@@ -40,7 +40,8 @@ class NewVisitorTest(unittest.TestCase):
     table = self.browser.find_element_by_id('id_list_table')
     rows = table.find_elements_by_tag_name('tr')
     self.assertTrue(
-        any(row.text == '1: Buy peacock feathers' for row in rows)
+        any(row.text == '1: Buy peacock feathers' for row in rows),
+        "New to-do item did not appear in table"
     )
 
     # There is still a text box inviting her to add another item. She
@@ -50,6 +51,7 @@ class NewVisitorTest(unittest.TestCase):
 
     # The page updates again, and now shows both items on her list
     # [...]
+
 
 if __name__ == '__main__':
   unittest.main()
--- django-test/ch04-philosophy-and-refactoring/superlists/lists/tests.py
+++ django-test/ch04-philosophy-and-refactoring/superlists/lists/tests.py
@@ -16,7 +16,7 @@ class HomePageTest(TestCase):
     request = HttpRequest()
     response = home_page(request)
     # print(repr(response.content))
-    self.assertTrue(response.content.startswith(b'<html>'))
+    self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
     self.assertIn(b'<title>To-Do lists</title>', response.content)
     self.assertTrue(response.content.strip().endswith(b'</html>'))
 
