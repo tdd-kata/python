4975b78a2dcee2ec9e7cd8b7d9569c978c51c7ce
Changsu Im <imcxsu@gmail.com>
2022-09-15 02:53:54 +0900

    baejoon base

---
+++ dsa/baekjoon.py
@@ -0,0 +1,11 @@
+# python3 baekjoon.py
+
+input_int = input("숫자 2개를 공백으로 구분해서 입력하세요. (example:1 3) >>>")
+a, b = map(int, input_int.split())
+print(a + b)
+
+"""
+https://help.acmicpc.net/language/info
+
+python3 -c "import py_compile; py_compile.compile(r'Main.py')"
+"""
