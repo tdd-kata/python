6a5b0c944693bd227b33e918144246f59ebc9e65
Changsu Im <imcxsu@gmail.com>
2022-09-21 00:06:20 +0900

    refactor generator

--- lang/generator.py
+++ lang/generator.py
@@ -2,11 +2,13 @@
 watch -n 0.1 pytest -v generator.py
 """
 
+
 # https://dojang.io/mod/page/view.php?id=2412
 def number_generator():
     yield 0
     yield 1
     yield 3
+
 
 def test_generator_is_iterator():
     gen = number_generator()
@@ -14,7 +16,13 @@ def test_generator_is_iterator():
     assert "__iter__" in sut
     assert "__next__" in sut
 
+
 def test_loop_list():
+    """
+    https://youtu.be/B8TAMOk-iD0
+    - 메모리 절약
+    - Lazy evaluation
+    """
     gen = number_generator()
 
     a = next(gen)
