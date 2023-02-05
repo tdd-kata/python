a02a79ecf5a044dbc7f5cbe6e1c150cb5d5beb29
Changsu Im <imcxsu@gmail.com>
2023-02-05 10:22:49 +0900

    update common module

--- disposable-scraper/robots/robots.py
+++ disposable-scraper/robots/robots.py
@@ -11,3 +11,19 @@ def exclusion_standard(robots_url: str) -> RobotFileParser:
     parser.set_url(robots_url)
     parser.read()
     return parser
+
+
+def validate(
+    robots_url: str,
+    init_url: str,
+    useragent: str = "*",
+) -> bool:
+    """
+    Validate URL by robots.txt.
+    """
+
+    parser = exclusion_standard(robots_url)
+    if not parser.can_fetch(useragent=useragent, url=init_url):
+        raise PermissionError("Cannot fetch")
+
+    return True
